7c3f4cd7b9c87cf2046e7c27ff38ebbe713717c4
Nicholas Humfrey <njh@aelius.com>
2018-02-19 20:08:45 +0000

    Added some extra specs

--- spec/track_spec.rb
+++ spec/track_spec.rb
@@ -2,7 +2,20 @@ require File.dirname(__FILE__) + "/spec_helper.rb"
 require 'track'
 
 describe Track do
-  it "should allow you to store and retrieve the track title" do
-    expect(Track.new(:track_title => 'foo').track_title).to eq('foo')
+  let(:track) {
+    Track.new(:performer => 'Jamie XX', :track_title => 'Loud Places')
+  }
+
+  it "should allow you to retrieve track_title" do
+    expect(track.track_title).to eq('Loud Places')
+  end
+
+  it "should allow you to retrieve the performer" do
+    expect(track.performer).to eq('Jamie XX')
+  end
+
+  it "should allow you to change the track_title" do
+    track.track_title = 'The Rest Is Noise'
+    expect(track.track_title).to eq('The Rest Is Noise')
   end
 end
